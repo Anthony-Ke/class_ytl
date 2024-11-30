9b89a2777d5548909e9e37933fe47276d741bf5b
IsNothingness <me3601523132@outlook.com>
2024-11-30 16:19:43 +0800

    1

--- lesson_1/practice.py
+++ lesson_1/practice.py
@@ -10,7 +10,8 @@ print(resident_list[0].keys())
 # 创建一个字典 resident_dict 展示前100个居民的属性，
 # 以居民的"id"为key，对应的value为一个字典。
 # 这个字典里面包含了姓名，性别，house_ue_id，职业这些信息
-
+a = 1
+b = 2
 # 定位出前100个居民
 # 创建一个字典，表示每一个"house_ue_id"对应了几个居民
 # 利用这个字典，找出住的人最多的"house_ue_id"，不需要体现是否并列第一
