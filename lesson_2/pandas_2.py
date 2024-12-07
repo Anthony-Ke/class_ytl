3655c755fb4036cf750932f4b671ec0447e0adb6
z5365195Yuhao <804779841@qq.com>
2024-12-07 16:01:01 +0800

    lesson 2 complete

--- lesson_2/pandas_2.py
+++ lesson_2/pandas_2.py
@@ -5,8 +5,9 @@ df = pd.read_csv('./population.csv')
 """
 group by 方法
 """
+# .groupby()会产生一个可便利的对象，这里面每一个元素都是一个包含分类值和对应df的tuple
 # for group in df.groupby('age'):
-#     print(group)
+#     print(group[1])
 
 # grouped_df = df.groupby('age')
 #
@@ -17,8 +18,21 @@ group by 方法
 """
 将居民按照住的楼栋分组，打印出每一组里面各个age的人有多少
 """
+df["building_number"] = df["address"].apply(lambda x:x.split("栋")[0])
+print(df.head())
 
-df.groupby('age').size()
+df_grouped_by_building = df.groupby("building_number")
+# name 为值 group为对应的小df
+for name, group in df_grouped_by_building:
+    print(name)
+    print(group.iloc[0:2])
+
+"""
+home_work_12_7
+"""
+
+# 尝试
+# df.groupby('age').size()
 
 
 """
