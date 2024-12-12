579f42abe7864ddd106760869fe564cc1864b9a1
IsNothingness <me3601523132@outlook.com>
2024-12-12 20:36:08 +0800

    12.12

--- lesson_2/numpy_pandas_1.py
+++ lesson_2/numpy_pandas_1.py
@@ -95,19 +95,33 @@ get column
 """
 找出不同age各有多少人 和 各个gender有多少人 
 """
+# 读取文件
 df_population = pd.read_csv(".\lesson_2\population.csv")
-resultDict = {}
+# 获取数据，然后转化为list
 ageList = df_population["age"].tolist()
+genderList = df_population["gender"].tolist()
+#
 ageResultDict = {}
 genderResultDict = {}
-genderList = df_population["gender"].tolist()
+# 初始化一个最终的list
+resultDict = {}
+# 同时遍历age和gender，故采取person的
 for personIndex in range(len(ageList)):
+    # 当这个age已经在result中
     if ageList[personIndex] in ageResultDict.keys():
+        # 把对应的人数统计起来，故都加一
         ageResultDict[ageList[personIndex]] += 1
+    # 当这个age不在result中
+    else:
+        # 就新建并初始化为一
+        ageResultDict[ageList[personIndex]] = 1
+    if genderList[personIndex] in genderResultDict.keys():
+        # 把对应的人数统计起来，故都加一
         genderResultDict[genderList[personIndex]] += 1
     else:
-        ageResultDict[ageList[personIndex]] = 1
+        # 当这个gender不在result中
         genderResultDict[genderList[personIndex]] = 1
+# 存储为一个字典
 resultDict = {"age":ageResultDict,"gender":genderResultDict}
 print(resultDict)
 """
@@ -122,17 +136,25 @@ get element of dataframe
 """
 find the student with the highest math grade
 """
-studentList = df_student["ID"].tolist()
+# 读取dataframe为list
+IDList = df_student["ID"].tolist()
 mathGradeList = df_student["Math_Grade"].tolist()
+# 分数从0到100，所以初始值为-1
 higestGrade = -1
+# 新建列表包含所有最高分学生的list
 studentHaveHigestGrade = []
-for personIndex in range(len(studentList)):
+for personIndex in range(len(IDList)):
+    # 出现新的最高分
     if mathGradeList[personIndex] > higestGrade:
+        # 清除记录的list
+        #（一般可以直接赋值空值来清空数组,但是clear会是修改内存，重新赋值则是在内存中再建立一块）
         studentHaveHigestGrade.clear()
-        studentHaveHigestGrade.append(studentList[personIndex])
+        studentHaveHigestGrade.append(IDList[personIndex])
         higestGrade = mathGradeList[personIndex]
+    # 出现了一样的最高分
     elif mathGradeList[personIndex] == higestGrade:
-        studentHaveHigestGrade.append(studentList[personIndex])
+        # 添加记录
+        studentHaveHigestGrade.append(IDList[personIndex])
 print(f"{studentHaveHigestGrade} have  the higest grabe on math ({higestGrade})")
 
 
@@ -142,6 +164,10 @@ print(f"{studentHaveHigestGrade} have  the higest grabe on math ({higestGrade})"
 """
 add new row
 """
+#相当于添加一堆键值对
+#可以传入一个dict
+#还可以用list来进行一些额外操作
+#一般在遍历等里面常用
 # df_3 = df_3._append({"A":10, "B":20, "C":30, "D":40, "E":40},ignore_index=True)
 # #ignore_index = True 不可省略
 # print(df_3)
@@ -150,10 +176,10 @@ add new row
 添加一行展示各个学科的平均分,identity, gender, class number全部写 "All"
 """
 
-
 """
 add new column
 """
+# 获取的是行数用len的话
 # length = len(df_3)
 # df_3["F"] = [random.choice(range(100)) for i in range(length)]
 # print(df_3.head())
@@ -161,13 +187,29 @@ add new column
 """
 apply method
 """
+# 对一个panda series进行同样操作
 # df_3["A"] = df_3["A"].apply(lambda x : x * 2)
 # print(df_3.head()["A"])
 
 """
 添加一列表现学生的数学成绩和平均分的差值
 """
-
+#加载数学的平均成绩并且初始化差值
+allMathGrade = df_student["Math_Grade"].tolist()
+averageMathGrade = sum(allMathGrade)/len(allMathGrade)
+differenceMathGradeList = []
+#获取行数
+length = len(df_student)
+#加对应行数的list，用来存放差值
+for studentNumber in range(length):
+    #计算差值
+    differenceMathGrade = df_student["Math_Grade"].iloc[studentNumber] - averageMathGrade
+    #添加到数组
+    differenceMathGradeList.append(differenceMathGrade)
+df_student["Difference_In_Math"] = differenceMathGradeList
+print(df_student.head) 
 """
 将最终的df写成csv的格式
 """
+df_student.to_csv("outpu.csv",index = False)
+# index = False 防止csv出错,除非index有名字，不然加上能爬避免打印index
--- lesson_2/pandas_2.py
+++ lesson_2/pandas_2.py
@@ -1,10 +1,11 @@
 import pandas as pd
 
-df = pd.read_csv('./population.csv')
+df = pd.read_csv('.\lesson_2\population.csv')
 
 """
 group by 方法
 """
+# 将会给产生一个可遍历的对象，每一个元素都是包含分类具体值，dataframe
 # for group in df.groupby('age'):
 #     print(group)
 
@@ -17,6 +18,31 @@ group by 方法
 """
 将居民按照住的楼栋分组，打印出每一组里面各个age的人有多少
 """
+#初始化存储所以最终数据的dict
+ageDictByHouse = {}
+#通过address分组，得到tuple里面0为对应的房子，1为这个房子内人的df数据，对这个遍历
+for groupByHouse in df.groupby("address"):
+    #获取对应数据
+    house = groupByHouse[0]
+    persons = groupByHouse[1]
+    #初始化单独房间内的dict
+    ageDictInHouse = {}
+    #遍历房间中的人
+    for lineNumber in range(len(persons)):
+        age = persons["age"].iloc[lineNumber]
+        #判断年龄是否记录
+        if age in ageDictInHouse:
+            #当年龄已经记录，则记录加一
+            ageDictInHouse[age] += 1
+        else:
+            #没被记录，则记录为一
+            ageDictInHouse[age] = 1
+    #加入到整体大dict里面
+    ageDictByHouse[house] = ageDictInHouse
+#遍历输出结果
+for item in ageDictByHouse.items():
+    print(item)
+
 
 df.groupby('age').size()
 
